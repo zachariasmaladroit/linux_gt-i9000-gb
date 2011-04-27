79d36461f7095406b1ed875e25c72661528c6273
ytt3r <3lvam91r0@gmail.com>
2011-04-27 19:18:58 -0400

    Releasetools should be fully working now.  Switched to bmlwrite instead of redbend_ua, it's opensource.

--- buildcaptivate.sh
+++ buildcaptivate.sh
@@ -1,7 +1,7 @@
 #Script to build for the captivate and zip the package.
 make aries_captivate_defconfig
 make -j`grep 'processor' /proc/cpuinfo | wc -l`
-cp /arch/arm/boot/zImage releasetools/updates
+cp /arch/arm/boot/zImage releasetools
 cd releasetools
 zip * Talon
 echo "bam. done."
