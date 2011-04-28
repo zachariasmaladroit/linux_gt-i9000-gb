dc6ea45eb8420cb7de1c99493f50e32493d76eaf
existz <robbeane@gmail.com>
2011-04-27 21:24:35 -0400

    Updated build script for Captivate

--- buildcaptivate.sh
+++ buildcaptivate.sh
@@ -1,7 +1,9 @@
-#Script to build for the captivate and zip the package.
-make aries_captivate_defconfig
+# Script to build for the captivate and zip the package.
+# Written by Evan alias ytt3r
+make aries_talon_defconfig
+export LOCALVERSION="-I9000XWJVB-CL118186"
 make -j`grep 'processor' /proc/cpuinfo | wc -l`
-cp /arch/arm/boot/zImage releasetools
+cp arch/arm/boot/zImage releasetools
 cd releasetools
 zip * Talon
-echo "bam. done."
+echo "Finished."
