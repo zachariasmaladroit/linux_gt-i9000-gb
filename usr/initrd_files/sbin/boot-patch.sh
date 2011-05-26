5e14c0c96128386c4e644b6792bcc0eae53556b9
kodos96 <kevin.t.bowen@gmail.com>
2011-05-26 13:07:13 -0700

    zram/lmk: consolidated lmk/zram settings into a single ram.conf file, added a new default preset

--- usr/initrd_files/sbin/boot-patch.sh
+++ usr/initrd_files/sbin/boot-patch.sh
@@ -5,9 +5,8 @@
 /sbin/ext/busybox sh /sbin/boot/properties.sh
 /sbin/ext/busybox sh /sbin/boot/install.sh
 /sbin/ext/busybox sh /sbin/boot/ext4speed.sh
-/sbin/ext/busybox sh /sbin/boot/zram.sh
+/sbin/ext/busybox sh /sbin/boot/ramset.sh
 /sbin/ext/busybox sh /sbin/boot/tweaks.sh
-/sbin/ext/busybox sh /sbin/boot/lmkset.sh
 /sbin/ext/busybox sh /sbin/boot/scripts.sh
 /sbin/ext/busybox sh /sbin/boot/install_bln_liblights.sh
 /sbin/ext/busybox sh /sbin/boot/remountsystemrw.sh
