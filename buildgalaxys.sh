a002a21a7c5680e9a427c80f089d607665010c8d
existz <robbeane@gmail.com>
2011-06-04 14:10:01 -0400

    add build-script for galaxys

---
+++ buildgalaxys.sh
@@ -0,0 +1,34 @@
+# Script to build for galaxy s (i9000) and zip the package.
+# Written by Evan alias ytt3r
+# modified by kodos96
+
+make aries_eur_talon_defconfig
+
+#export LOCALVERSION="-I9000XWJVB-CL118186"
+export LOCALVERSION="-I9000XWJVH-CL184813"
+export KBUILD_BUILD_VERSION="0.2.1-talon-dev"
+export INSTALL_MOD_PATH=./mod_inst
+make modules -j`grep 'processor' /proc/cpuinfo | wc -l`
+make modules_install
+
+if [ -e ./usr/initrd_files/lib ]; then
+ rm -rf ./usr/initrd_files/lib
+fi
+
+mkdir ./usr/initrd_files/lib
+mkdir ./usr/initrd_files/lib/modules
+
+for i in `find mod_inst -name "*.ko"`; do
+ cp $i ./usr/initrd_files/lib/modules/
+done
+
+rm -rf ./mod_inst
+unzip ./usr/prebuilt_ko.zip -d ./usr/initrd_files/lib/modules/
+
+make -j`grep 'processor' /proc/cpuinfo | wc -l`
+cp arch/arm/boot/zImage releasetools
+cd releasetools
+rm -f *.zip
+zip -r Talon *
+cd ..
+echo "Finished."
