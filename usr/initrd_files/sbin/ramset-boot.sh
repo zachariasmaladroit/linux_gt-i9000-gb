fae2dafe2a08dfa74e4f92eb17c8cafe9b05135b
kodos96 <kevin.t.bowen@gmail.com>
2011-06-06 12:05:37 -0700

    voodoo-based initram (xcal-jvp)

--- usr/initrd_files/sbin/ramset-boot.sh
+++ usr/initrd_files/sbin/ramset-boot.sh
@@ -1,6 +1,6 @@
-#!/sbin/ext/busybox sh
+#!/bin/busybox sh
 
-/sbin/ext/busybox sh /sbin/boot/ramset.sh
+/bin/busybox sh /sbin/ramset.sh
 
 read sync < /data/sync_fifo_ramset
 rm /data/sync_fifo_ramset
