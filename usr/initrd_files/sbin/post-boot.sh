cb499ba7c36522f3eaa1ecb3c2a7fad3114d696a
kodos96 <kevin.t.bowen@gmail.com>
2011-07-01 17:27:21 -0700

    updated init.d blacklist in updater-script, added blacklist enforcement to post-boot

--- usr/initrd_files/sbin/post-boot.sh
+++ usr/initrd_files/sbin/post-boot.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
-#create busybox symlinks
-DIR=`pwd`
 /voodoo/bin/remount_system_rw
+
+# create busybox symlinks
+DIR=`pwd`
 if test -e /system/xbin/busybox; then
  cd /system/xbin
  for i in $(/system/xbin/busybox --list)
@@ -13,17 +14,27 @@ if test -e /system/xbin/busybox; then
  /system/xbin/busybox chmod +x /system/xbin/ll
  cd $DIR
 fi
+
+# delete "considered harmful" scripts
+cd /system/etc/init.d
+for i in `grep -l lowmemorykiller *` S90scheduler S99finish S98system_tweak 89system_tweak S98screenstate_scaling 90screenstate_scaling S97ramscript S97rambooster; do
+ if test -e $i; then rm $i;fi;
+done
+cd $DIR
+
 /voodoo/bin/remount_system_ro
+
+
 
 # run the init.d scripts first, so that if anything in them conflicts with our
 # settings, ours win ;)
 /bin/sh /voodoo/scripts/run-parts.sh /system/etc/init.d
 
-#clean up old cf-root tweak properties
+# clean up old cf-root tweak properties
 /system/xbin/busybox rm /data/property/persist.tweak*
 
 
-#disable iostats to reduce overhead
+# disable iostats to reduce overhead
 STL=`ls -d /sys/block/stl*`;
 BML=`ls -d /sys/block/bml*`;
 MMC=`ls -d /sys/block/mmc*`;
