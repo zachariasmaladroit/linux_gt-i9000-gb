7ebf0706ee341bdd43dc95f329b0963fd498c02c
Matt B <jackdachef@gmail.com>
2011-07-22 01:15:43 +0200

    snail12_t3

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ if ! [ -e .config ]; then
  make $1
 fi
 
-export KBUILD_BUILD_VERSION="0.4.2-talon-dev_snail12"
+export KBUILD_BUILD_VERSION="0.4.2-talon-dev_snail12_t3"
 
 #export LOCALVERSION="-I9000XWJVB-CL118186"
 #export LOCALVERSION="-I9000XWJVH-CL184813"
